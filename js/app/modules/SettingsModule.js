d831af2e87318830c656e0c4dbb6f2c73d706d31
Thiago Nascimento <thiagodnf@gmail.com>
2016-12-11 10:48:39 -0200

    added import and export data

--- js/app/modules/SettingsModule.js
+++ js/app/modules/SettingsModule.js
@@ -4,6 +4,8 @@ var SettingsModule  = {
 
         $("#settings-language").val(DatabaseUtils.get("language") || "en-US");
 
+        var lang = (DatabaseUtils.get("language") || "en-US").substring(0, 2);
+
         $.validate({ form:"#form-settings", onSuccess: function($form) {
 
             DatabaseUtils.set("language", $("#settings-language").val());
@@ -17,17 +19,112 @@ var SettingsModule  = {
             return false;
         }});
 
+        $("#btn-settings-clear-all").click(function(event){
+            event.preventDefault();
+
+            if(confirm("Are you sure?".toLocaleString())){
+                localStorage.clear();
+                location.reload();
+            };
+
+            return false;
+        });
+
+        $("#btn-settings-export").click(function(event){
+            event.preventDefault();
+
+            var keys = {}
+
+            for (var key in localStorage){
+               keys[key] = {};
+            }
+
+            for(var i in keys){
+                keys[i] = localStorage.getItem(i);
+            }
+
+            var str = JSON.stringify(keys);
+
+            var blob = new Blob([str], {type: "text/plain;charset=utf-8"});
+
+    		var a = document.createElement("a");
+    		document.body.appendChild(a);
+        	a.style = "display: none";
+    		a.href = window.URL.createObjectURL(blob);
+    		a.download = "export.json";
+    		a.click();
+
+            return false;
+    	});
+
+        $.validate({ form:"#form-import",  lang : lang, onSuccess: function($form) {
+            that.readFile();
+        }});
+
+        $("#dbtn-import").click(function(event){
+            event.preventDefault();
+
+            that.readFile();
+
+            return false;
+        });
+
         this.reload();
     },
     actions: [
         "done"
     ],
-    reload: function(){
-        String.locale = DatabaseUtils.get("language");
+    readFile: function() {
+        var that = this;
 
+        var files = document.getElementById('files').files;
+
+        if ( ! files.length) {
+            alert('Please select a file!');
+            return;
+        }
+
+        var file = files[0];
+
+        var reader = new FileReader();
+
+        // If we use onloadend, we need to check the readyState.
+        reader.onloadend = function(evt) {
+            if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+                that.importJSONString(evt.target.result);
+            };
+        }
+
+        var blob = file.slice(0, file.size);
+        reader.readAsBinaryString(blob);
+    },
+    importJSONString: function(str){
+        var obj = JSON.parse(str);
+
+        for(var i in obj){
+            localStorage.setItem(i, obj[i]);
+        }
+
+        alert("Successfully Imported");
+
+        location.reload();
+    },
+    reload: function(){
         $("a[href='#settings-tab-general']").text("General".toLocaleString());
+        $("a[href='#settings-tab-advanced']").text("Advanced".toLocaleString());
+
         $("label[for='settings-language']").text("Language".toLocaleString());
+        $("p[for='settings-data']").text("Data".toLocaleString());
+        $("p[for='settings-reset']").text("Reset Settings".toLocaleString());
+        $("p[for='settings-reset-warning']").text("Warning! This cannot be undone!".toLocaleString());
+
         $("#btn-settings-save").text("Save".toLocaleString());
+        $("#btn-settings-clear-all").text("Clear All".toLocaleString());
+        $("#btn-settings-import").text("Import".toLocaleString());
+        $("#btn-settings-export").text("Export".toLocaleString());
+        $("#btn-import").text("Import".toLocaleString());
+        $("#btn-cancel").text("Cancel".toLocaleString());
+        $("#modal-import .modal-title").text("Import".toLocaleString());
 
         $("option[value='en-US']").text("English".toLocaleString());
         $("option[value='pt-BR']").text("Portuguese".toLocaleString());
--- js/script.js
+++ js/script.js
@@ -31,6 +31,15 @@ $(function(){
         String.locale = "en-US";
     }
 
+    console.log(String.locale)
+
+    // Check for the various File API support.
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        console.log("Great success! All the File APIs are supported.");
+    } else {
+        alert('The File APIs are not fully supported in this browser.');
+    }
+
     $(".navbar-brand").text("Shopping List".toLocaleString());
     document.title  = "Shopping List".toLocaleString();
 
