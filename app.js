0391cb0aca4125e7d96889f99d56f0c411032329
Thiago Ferreira <thiagodnf@gmail.com>
2022-12-11 13:49:40 -0500

    updated libraries and removed old cache library

--- app.js
+++ app.js
@@ -7,28 +7,6 @@ var connect = require('connect');
 
 connect().use(serveStatic(__dirname)).listen(port);
 
-recursive('./', ['.git','node_modules','.editorconfig','.gitignore','app.js','cache.appcache','LICENSE','package.json','README.md','update-gh-pages.sh'], function (err, files) {
-    //all = all.concat(files);
-
-    var stream = fs.createWriteStream("cache.appcache");
-    stream.once('open', function(fd) {
-        stream.write("CACHE MANIFEST \n");
-        stream.write("# Timestamp "+new Date().getTime()+"\n");
-
-        stream.write("\n");
-
-        for(var i = 0; i < files.length; i++){
-            stream.write((files[i].replace(/\\/g,'/')) +" \n");
-        }
-
-        stream.write("\n");
-        stream.write("NETWORK: \n");
-        stream.write("* \n");
-
-        stream.end();
-    });
-});
-
 function replaceAll(str, find, replace) {
   return str.replace(new RegExp(find, 'g'), replace);
 }
